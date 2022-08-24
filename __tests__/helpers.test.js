b105e237d259acc235154bb043ed144723cbb30c
Theresa2013 <tab61295@gmail.com>
2022-08-23 21:35:20 -0500

    add delete and edit post functions

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -5,21 +5,3 @@ test('format_date() returns a date string', () => {
 
     expect(format_date(date)).toBe('3/20/2020');
 });
-
-test('format_plural() correctly pluralizes words', () => {
-    const word1 = format_plural('tiger', 1);
-    const word2 = format_plural('lion', 2);
-
-    expect(word1).toBe('tiger');
-    expect(word2).toBe('lions');
-});
-
-test('format_url() returns a simplified url string', () => {
-    const url1 = format_url('http://test.com/page/1');
-    const url2 = format_url('https://www.coolstuff.com/abcdefg/');
-    const url3 = format_url('https://www.google.com?q=hello');
-
-    expect(url1).toBe('test.com');
-    expect(url2).toBe('coolstuff.com');
-    expect(url3).toBe('google.com');
-});
